f324ce57de9e8c058c3ad83ce1352c7f152f7ea5
Craig Wells <cwells@northplains.com>
2016-04-11 15:38:49 +0100

    new repo

--- www/js/script-angular.js
+++ www/js/script-angular.js
@@ -4,6 +4,19 @@
 
     .controller('MainCtrl', ['$scope', function($scope) {}])
 
+    .controller('viewCtrl', ['$scope', function($scope) {
+        // https://www.w3.org/TR/2dcontext/#the-canvas-state
+        var Canvas = document.getElementById('mainCanvas');
+        var context = Canvas.getContext("2d");
+        console.log(context);
+
+        context.beginPath();
+        context.moveTo(0,0);
+        //context.setLineDash([1, 10, 5, 10, 1, 5, 10, 5])
+        context.lineTo(200,200);
+        context.stroke();
+    }])
+
     /* Declare the graph directive as Element type */
     someApp.directive('canvasContainer', function() {      
         return {
